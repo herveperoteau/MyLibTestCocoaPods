a5a506b291e6c871c6a4a6f876b99b965a1b773d
Herve Peroteau <herve.peroteau@gmail.com>
2013-06-03 14:01:10 +0200

    Initial Commit

---
+++ MyLibTestCocoaPods/MyLibTestCocoaPods.h
@@ -0,0 +1,13 @@
+//
+//  MyLibTestCocoaPods.h
+//  MyLibTestCocoaPods
+//
+//  Created by Hervé PEROTEAU on 03/06/13.
+//  Copyright (c) 2013 Hervé PEROTEAU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyLibTestCocoaPods : NSObject
+
+@end
---
+++ MyLibTestCocoaPods/MyLibTestCocoaPods.m
@@ -0,0 +1,13 @@
+//
+//  MyLibTestCocoaPods.m
+//  MyLibTestCocoaPods
+//
+//  Created by Hervé PEROTEAU on 03/06/13.
+//  Copyright (c) 2013 Hervé PEROTEAU. All rights reserved.
+//
+
+#import "MyLibTestCocoaPods.h"
+
+@implementation MyLibTestCocoaPods
+
+@end
